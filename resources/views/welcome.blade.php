ed3b9680d4148934d4ba7fbbb2fdee735a8027ac
rgomide <renato.s.gomide@gmail.com>
2023-03-22 12:47:30 -0300

    update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,10 +8,19 @@
     <title>Laravel</title>
 </head>
 
-<body class="antialiased">
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+<body>
+    <!-- Global Container -->
+    <div class="flex items-center justify-center min-h-screen bg-gray-50">
+        <!-- Card Container -->
+        <div
+            class="relative flex flex-col m-6 space-y-10 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0 md:m-0">
+            <div class="p-6 md:p-20">
+                <h1 class="text-3xl font-bold">
+                    Hello world!
+                </h1>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
